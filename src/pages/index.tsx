20cb9d697411f0ccfce88f61eaed5dbe6ad61c18
Tiago Andrade <tailergamer7@gmail.com>
2021-03-22 16:17:51 -0400

    :sparkles: feat(lesson: Title dinâmico por página): create a dynamic title per page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head';
+
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello <span>World</span>
-    </h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
+    </>
   )
 }
