4bbde8805999e949fd6b648a2b00985705e3b312
Tiago Andrade <tailergamer7@gmail.com>
2021-03-26 18:05:57 -0400

    :sparkles: feat(lesson: Redirecionando para o Stripe): create a redirect when the user click in the subscribe button

--- src/components/SubscibeButton/index.tsx
+++ src/components/SubscibeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscibeButtonProps {
@@ -7,18 +9,31 @@ interface SubscibeButtonProps {
 
 export function SubscibeButton({ priceId }: SubscibeButtonProps) {
   const [session] = useSession();
-  
-  function handleSubscribe() {
+
+  async function handleSubscribe() {
     if (!session) {
       signIn('github')
       return;
-    }    
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscibe now
     </button>
