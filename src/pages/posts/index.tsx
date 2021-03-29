0e1c6048a804e0e3c8b05310bb87520f69e536cd
Tiago Andrade <tailergamer7@gmail.com>
2021-03-28 21:10:15 -0400

    :sparkles: feat(lesson: Consumindo API do Prismic): use ssg to consume the prismic api

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,9 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import styles from './styles.module.scss'
+import Prismic from '@prismicio/client';
+
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
 
 export default function Posts() {
   return (
@@ -29,4 +33,21 @@ export default function Posts() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  });
+
+  
+
+  return {
+    props: {}
+  }
 }
