6c1135b87d4edd53cfca379f4bcb86b0d8c23fef
Tiago Andrade <tailergamer7@gmail.com>
2021-06-02 18:58:37 -0400

    🧪 test: add coverage report

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,15 @@ module.exports = {
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(scss|css|sass)$": "identity-obj-proxy"
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx",
+    "!src/**/_app.tsx",
+    "!src/**/document.tsx"
+  ],
+  coverageReporters: [
+    "lcov", "json"
+  ]
 };
